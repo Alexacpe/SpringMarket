315ee9a71372873f8de5657d172c9b3d196ebdc7
Alex Acevedo <alex.acpe@outlook.com>
2022-07-11 19:29:44 -0500

    Implementando query methods y utilizar la anotacion Repository

--- src/main/java/com/spring/market/persistence/ProductoRepository.java
+++ src/main/java/com/spring/market/persistence/ProductoRepository.java
@@ -2,14 +2,38 @@ package com.spring.market.persistence;
 
 import com.spring.market.persistence.crud.ProductoCrudRepository;
 import com.spring.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository//Es indicar que desde esta clase es desde donde se realizan las operaciones en la base de datos
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
-
-    //metodo que recupera la lista de todos los productos
+        //Metodos para interactuar con la base de datos
+    /**recupera la lista de todos los productos*/
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    /**recuperar la lista de productos por categoria*/
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    /**traer lista de productos escasos*/
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+    /**consultar un producto en particular*/
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);//findByID:recuperar un elemento desde si PK
+    }
+    /**Guardar producto*/
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    /**Eliminar el producto*/
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
--- src/main/java/com/spring/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/spring/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,15 @@ package com.spring.market.persistence.crud;
 import com.spring.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 //Interface que interactua con Producto
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {//crudrepository recibe la tabla y el tipo de dato de la clave primaria
+        // QUERY METHODS: se realizan las consultas a la base de datos sin necesidad de ejecutar los query de SQL
+    /**Recuperar la lista de productos que pertenecen a una categoria en orden ascendente por nombre*/
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    /**Traer la lista de los productos que se están agotando en stock que sean menor que*/
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
+
 }

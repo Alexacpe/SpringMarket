06e7842e97e7352d5a1b48ea081b3dabc11cd76e
Alex Acevedo <alex.acpe@outlook.com>
2022-07-11 23:05:43 -0500

    Dependencia de map struct, creacion del dominio y su interface

---
+++ src/main/java/com/spring/market/domain/repository/ProductRepository.java
@@ -0,0 +1,36 @@
+package com.spring.market.domain.repository;
+
+//Dar las reglas de comportamiento a todos los repositorios cuando se hable en terminos de producto, asi intercatuar en base al dominio y no a los repositorios que van directo a la base de datos
+
+import com.spring.market.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    //metodos que quiero que cualquier repositorios que vaya a trabajar con productos tenga que implementar,
+    // asi trabajar con direccion al dominio y no exponer las clases con nombres de las tablas de la base de datos
+    /**Retornar una lista de todos los productos*/
+    List<Product> getAll();
+    /**
+     *Retornar productos por categoria
+     * @param categoryId:id de cada producto
+     */
+    Optional<List<Product>> getByCategory(int categoryId);
+
+    /** Traer los productos que estan ecasos
+     * @param quantity: asignar la cantidad que se considere para que el producto cuente como escaso
+     * */
+    Optional<List<Product>> getScarseProducts(int quantity);
+
+    /**Retornar un producto específico
+     * @param productId: traer el producto por su id
+     */
+    Optional<Product> getProduct(int productId);
+    /**Guardar producto
+     * @param product: objeto de tipo product completo
+     */
+    Product save(Product product);
+
+    void delete(int productId);
+}
